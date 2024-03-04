81ce38a3f2459c19b17efa34c9eae5f4370992b8
fatmasyifa <88578127+fatmasyifa@users.noreply.github.com>
2024-03-04 18:44:19 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,3 +1,4 @@
+STREAMLIT FIX
 
 import streamlit as st
 import pandas as pd
@@ -6,154 +7,154 @@ import seaborn as sns
 from statsmodels.tsa.seasonal import seasonal_decompose
 import numpy as np
 
-# Title page
-st.set_page_config(page_title="Air Quality from Wanshouxigong Analysis by maliki_borneo")
+
+st.set_page_config(page_title="Analisis Kualitas Udara di Aotizhongxin")
 
 
-# Load dataset
-data = pd.read_csv('./data/PRSA_Data_Aotizhongxin_20130301-20170228.csv')
-
-# Title of the dashboard
-st.title('Air Quality Analysis Dashboard: Wanshouxigong Station')
+file_path = 'https://raw.githubusercontent.com/fatmasyifa/myproject/main/PRSA_Data_Aotizhongxin_20130301-20170228.csv'
+data = pd.read_csv(file_path)
 
 
-
-# Description
-st.write('This dashboard provides an interactive way to explore air quality data, specifically focusing on PM2.5 levels and their relationship with various weather conditions.')
+st.title('Dashboard Analisis Kualitas Udara di Aotizhongxin')
 
 
-# About me
+
+
+st.write('Dashboard ini merupakan media untuk menyampaikan hasil analisis data kualitas udara di Aotizhongxin secara interaktif yang berfokus pada tingkat PM2.5 dan hubungannya dengan berbagai macam kondisi cuaca.')
+
+
+
 st.markdown("""
-### About Me
-- **Name**: Reza Maliki Akbar
-- **Email Address**: rezamaliki.akbar@gmail.com
-- **Dicoding ID**: [maliki_borneo](https://www.dicoding.com/users/maliki_borneo/)
+- **Name**: Fatimah Fatma Syifa
+- **Email**: fatmasyifa32@gmail.com
+- **Dicoding ID**: fatmasyifa
 
-### Project Overview
-This dashboard presents an analysis of air quality data, particularly focusing on PM2.5 levels, from the Wanshouxigong station. The project aims to uncover trends, seasonal variations, and the impact of different weather conditions on air quality. Insights from this analysis can be valuable for environmental studies and public health monitoring.
+Dashboard ini menunjukkan analisis data kualitas udara yang berfokus pada tingkat PM2.5 dari Aotizhongxin yang bertujuan untuk mengungkap kecenderungan, variasi per musim, dan kualitas udara yang diakibatkan perbedaan kondisi cuaca. Analisis ini berguna untuk studi lingkungan dan memantau kesehatan masyarakat.
 """)
 
-# Adding a sidebar for interactive inputs
+
 st.sidebar.header('User Input Features')
 
-# Let users select a year and month to view data
-selected_year = st.sidebar.selectbox('Select Year', list(data['year'].unique()))
-selected_month = st.sidebar.selectbox('Select Month', list(data['month'].unique()))
 
-# Filter data based on the selected year and month
+selected_year = st.sidebar.selectbox('Pilih Tahun', list(data['year'].unique()))
+selected_month = st.sidebar.selectbox('Pilih Bulan', list(data['month'].unique()))
+
+
 data_filtered = data[(data['year'] == selected_year) & (data['month'] == selected_month)].copy()
 
-# Displaying data statistics
-st.subheader('Data Overview for Selected Period')
+
+st.subheader('Data berdasarkan periode yang dipilih')
 st.write(data_filtered.describe())
 
-# Line chart for PM2.5 levels over selected month
-st.subheader('Daily PM2.5 Levels')
+
+st.subheader('Tingkat PM2.5 (harian)')
 fig, ax = plt.subplots()
 ax.plot(data_filtered['day'], data_filtered['PM2.5'])
-plt.xlabel('Day of the Month')
-plt.ylabel('PM2.5 Concentration')
+plt.xlabel('Hari dalam bulan')
+plt.ylabel('Konsentrasi PM2.5')
 st.pyplot(fig)
 
-# Correlation heatmap for the selected month
-st.subheader('Correlation Heatmap of Air Quality Indicators')
+
+st.subheader('Korelasi Heatmap dari indikator kualitas udara')
 corr = data_filtered[['PM2.5', 'NO2', 'SO2', 'CO', 'O3', 'TEMP', 'PRES', 'DEWP']].corr()
 fig, ax = plt.subplots()
 sns.heatmap(corr, annot=True, ax=ax)
-plt.title('Correlation Heatmap')
+plt.title('Korelasi Heatmap')
 st.pyplot(fig)
 
-# Seasonal Trend Analysis
-st.subheader('Seasonal Trend Analysis')
+
+st.subheader('Analisis Kecenderungan Per Musim')
 seasonal_trends = data.groupby('month')['PM2.5'].mean()
 fig, ax = plt.subplots()
 seasonal_trends.plot(kind='bar', color='skyblue', ax=ax)
-plt.title('Average Monthly PM2.5 Levels')
-plt.xlabel('Month')
-plt.ylabel('Average PM2.5')
+plt.title('Tingkat rata-rata bulanan PM2.5')
+plt.xlabel('Bulan')
+plt.ylabel('Rata-rata PM2.5')
 st.pyplot(fig)
 
 
-# Daily PM2.5 Levels
-st.subheader('Daily PM2.5 Levels')
+
+st.subheader('Tingkat 2.5 (harian)')
 fig, ax = plt.subplots()
 ax.plot(data_filtered['day'], data_filtered['PM2.5'])
-plt.xlabel('Day of the Month')
-plt.ylabel('PM2.5 Concentration')
+plt.xlabel('Hari dalam bulan')
+plt.ylabel('Konsentrasi PM2.5')
 st.pyplot(fig)
 
-# Pollutant Distribution
-st.subheader('Pollutant Distribution')
-selected_pollutant = st.selectbox('Select Pollutant', ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO'])
+
+st.subheader('Distribusi Polutan')
+selected_pollutant = st.selectbox('Pilih Polutan', ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO'])
 fig, ax = plt.subplots()
 sns.boxplot(x='month', y=selected_pollutant, data=data[data['year'] == selected_year], ax=ax)
 st.pyplot(fig)
 
-# Time Series Decomposition of PM2.5
-st.subheader('Time Series Decomposition of PM2.5')
+
+st.subheader('Dekomposisi Rangkaian Waktu PM2.5')
 try:
     data_filtered['PM2.5'].ffill(inplace=True)
-    decomposed = seasonal_decompose(data_filtered['PM2.5'], model='additive', period=24) # Adjust period as necessary
+    decomposed = seasonal_decompose(data_filtered['PM2.5'], model='aditif', period=24) # Adjust period as necessary
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
-    decomposed.trend.plot(ax=ax1, title='Trend')
-    decomposed.seasonal.plot(ax=ax2, title='Seasonality')
-    decomposed.resid.plot(ax=ax3, title='Residuals')
+    decomposed.trend.plot(ax=ax1, title='Kecenderungan')
+    decomposed.seasonal.plot(ax=ax2, title='Musim')
+    decomposed.resid.plot(ax=ax3, title='Residu')
     plt.tight_layout()
     st.pyplot(fig)
 except ValueError as e:
-    st.error("Unable to perform time series decomposition: " + str(e))
+    st.error("Tidak dapat menunjukkan dekomposisi rangkaian waktu: " + str(e))
 
 
-# Hourly Averages Heatmap
-st.subheader('Hourly Averages of PM2.5')
+
+st.subheader('Rata-rata Tingkat PM2.5 (jam)')
 try:
-    # Ensure correct data types and handle missing values
+    
     data['hour'] = data['hour'].astype(int)
     data['PM2.5'] = pd.to_numeric(data['PM2.5'], errors='coerce')
     data['PM2.5'].ffill(inplace=True)
 
-    # Calculate hourly averages
+    
     hourly_avg = data.groupby('hour')['PM2.5'].mean()
 
-    # Plotting
+    
     fig, ax = plt.subplots()
     sns.heatmap([hourly_avg.values], ax=ax, cmap='coolwarm')
-    plt.title('Hourly Averages of PM2.5')
+    plt.title('Rata-rata Tingkat PM2.5 (jam)')
     st.pyplot(fig)
 except Exception as e:
-    st.error(f"Error in plotting hourly averages: {e}")
+    st.error(f"Error: {e}")
 
-# Wind Direction Analysis
-st.subheader('Wind Direction Analysis')
+
+st.subheader('Analisis Arah Angin')
 wind_data = data_filtered.groupby('wd')['PM2.5'].mean()
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, polar=True)
 theta = np.linspace(0, 2 * np.pi, len(wind_data))
 bars = ax.bar(theta, wind_data.values, align='center', alpha=0.5)
-plt.title('PM2.5 Levels by Wind Direction')
+plt.title('Tingkat PM2.5 Berdasarkan Arah Angin')
 st.pyplot(fig)
 
-# Rainfall vs. Air Quality
-st.subheader('Rainfall vs. PM2.5 Levels')
+
+
+st.subheader('Curah Hujan vs Tingkat PM2.5')
 fig, ax = plt.subplots()
 sns.scatterplot(x='RAIN', y='PM2.5', data=data_filtered, ax=ax)
-plt.title('Rainfall vs. PM2.5 Levels')
+plt.title('Curah Hujan vs Tingkat PM2.5')
 st.pyplot(fig)
 
-# Correlation Heatmap - Interactive
-st.subheader('Interactive Correlation Heatmap')
-selected_columns = st.multiselect('Select Columns for Correlation', data.columns, default=['PM2.5', 'NO2', 'TEMP', 'PRES', 'DEWP'])
+
+st.subheader('Korelasi Heatmap Interaktif')
+selected_columns = st.multiselect('Pilih kolom untuk menentukan korelasi', data.columns, default=['PM2.5', 'NO2', 'TEMP', 'PRES', 'DEWP'])
 corr = data[selected_columns].corr()
 fig, ax = plt.subplots()
 sns.heatmap(corr, annot=True, ax=ax)
 st.pyplot(fig)
 
 
-# Conclusion
-st.subheader('Conclusion')
+
+
+st.subheader('Kesimpulan')
 st.write("""
-- The dashboard provides an in-depth and interactive analysis of air quality data.
-- Various visualizations offer insights into PM2.5 levels, their distribution, and factors affecting them.
-- Seasonal trends and the impact of different weather conditions and pollutants on air quality are clearly depicted.
-- Users can explore the data dynamically to gain a deeper understanding of air quality trends.
+- Dashboard memberikan analisis data kualitas udara yang lebih detail dan interaktif.
+- Berbagai visualisasi memberikan wawasan tentang tingkat PM2.5, distribusi tingkat PM2.5, dan faktor-faktor yang mempengaruhinya.
+- Kecenderungan per musim dan pengaruh berbagai kondisi cuaca dan polutan terhadap kualitas udara digambarkan dengan jelas.
+- User dapat menjelajahi data secara dinamis untuk mendapatkan pemahaman lebih mendalam tentang tren kualitas udara.
 """)
