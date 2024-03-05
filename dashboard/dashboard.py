f8ab6b662c7d626824aaa41848282402e44359ae
fatmasyifa <88578127+fatmasyifa@users.noreply.github.com>
2024-03-05 17:29:14 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -22,13 +22,7 @@ cols_to_plot = ['PM2.5', 'PM10']
 data_missing = df[cols_to_plot].isnull()
 data_missing['year'] = df['year']
 data_missing_2013 = data_missing[data_missing['year'] == 2013]
-plt.figure(figsize=(20, 8))
-sns.heatmap(data_missing_2013.drop('year', axis=1).T, cmap='viridis', cbar=False)
-plt.title('Missing Data Pattern for 2013')
-plt.xlabel('Date')
-plt.ylabel('Pollutant')
-plt.yticks(rotation=0)
-st.pyplot()
+st.pyplot(sns.heatmap(data_missing_2013.drop('year', axis=1).T, cmap='viridis', cbar=False))
 st.markdown("""
 Terdapat persentase data yang hilang untuk polutan relatif kecil, dengan PM2.5 yang hilang sekitar 2,63%, dan PM10 yang hilang sekitar 2,05%. Variabel polutan dan cuaca lainnya juga memiliki persentase data yang hilang dalam jumlah kecil, sehingga menunjukkan bahwa kumpulan data tersebut relatif lengkap. Heatmap tahun 2013 menunjukkan bahwa data yang hilang untuk PM2.5 dan PM10 tidak mengikuti pola yang jelas, sehingga menunjukkan bahwa hilangnya data tersebut mungkin terjadi secara acak atau tidak sistematis. Tidak ada data yang hilang dalam jangka panjang, yang merupakan pertanda baik untuk analisis deret waktu.
 """)
@@ -38,27 +32,14 @@ st.title("Time Series Analysis")
 data_imputed = df.fillna(method='ffill')
 data_imputed['date'] = pd.to_datetime(data_imputed[['year', 'month', 'day', 'hour']])
 data_time_series = data_imputed[['date', 'PM2.5', 'NO2']].set_index('date').resample('M').mean()
-plt.figure(figsize=(15, 6))
-plt.plot(data_time_series.index, data_time_series['PM2.5'], label='PM2.5', color='blue')
-plt.plot(data_time_series.index, data_time_series['NO2'], label='NO2', color='green')
-plt.title('Monthly Average Concentrations of PM2.5 and NO2')
-plt.xlabel('Date')
-plt.ylabel('Concentration')
-plt.legend()
-st.pyplot()
+st.line_chart(data_time_series)
 st.markdown("""
 Rata-rata bulanan dari PM2.5 dan NO2 dilihat dari grafik rata-rata konsentrasi bulanan dari PM2.5 dan NO2. Grafik di atas membantu untuk identifikasi 'seasonal trends' ataupun perubahan kualitas udara dari tahun ke tahun.
 """)
 
 # Plot Seasonal Trends
-plt.figure(figsize=(10, 6))
 seasonal_trends = data_imputed.groupby('month')['PM2.5'].mean()
-seasonal_trends.plot(kind='bar', color='skyblue')
-plt.title('Average PM2.5 Levels by Month')
-plt.xlabel('Month')
-plt.ylabel('Average PM2.5')
-plt.xticks(ticks=range(0, 12), labels=[str(m) for m in range(1, 13)], rotation=0)
-st.pyplot()
+st.bar_chart(seasonal_trends)
 st.markdown("""
 Diagram batang menunjukkan rata-rata tingkat PM2.5 dari setiap bulan, konsentrasi tertinggi terjadi pada bulan Desember, Maret (saat musim dingin) dan konsentrasi terendah terjadi pada bulan Agustus, September (saat musim kemarau)
 """)
