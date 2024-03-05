1b3c3e587e97c36ed86cf1161fa9cf22550d573c
fatmasyifa <88578127+fatmasyifa@users.noreply.github.com>
2024-03-05 16:31:51 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,10 +1,9 @@
-revisi streamlit
+
 
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from statsmodels.tsa.seasonal import seasonal_decompose
 import numpy as np
 
 
