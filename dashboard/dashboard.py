bc03a3609fb62d13e5823ec0625331de83260600
fatmasyifa <88578127+fatmasyifa@users.noreply.github.com>
2024-03-05 17:44:09 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,9 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+st.set_page_config(page_title="Analisis Kualitas Udara di Aotizhongxin")
+
 # Read data
 df = pd.read_csv('./data/PRSA_Data_Aotizhongxin_20130301-20170228.csv')
 
+st.title('Dashboard Analisis Kualitas Udara di Aotizhongxin')
+
+
+
+
+st.write('Dashboard ini merupakan media untuk menyampaikan hasil analisis data kualitas udara di Aotizhongxin secara interaktif yang berfokus pada tingkat PM2.5 dan hubungannya dengan berbagai macam kondisi cuaca.')
+
+
+
+st.markdown("""
+- **Name**: Fatimah Fatma Syifa
+- **Email**: fatmasyifa32@gmail.com
+- **Dicoding ID**: fatmasyifa
+
+Dashboard ini menunjukkan analisis data kualitas udara yang berfokus pada tingkat PM2.5 dari Aotizhongxin yang bertujuan untuk mengungkap kecenderungan, variasi per musim, dan kualitas udara yang diakibatkan perbedaan kondisi cuaca. Analisis ini berguna untuk studi lingkungan dan memantau kesehatan masyarakat.
+""")
 # Missing data analysis
 st.title("Missing Data Analysis")
 cols_to_plot = ['PM2.5', 'PM10']
